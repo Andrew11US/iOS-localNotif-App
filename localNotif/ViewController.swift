0a7f8a06aa3efc29da5196f3fe5fcd1d95240417
Andrii Halabuda <losAngelesBoy11@gmail.com>
2017-05-29 16:41:04 +0300

    Added Files

--- localNotif/ViewController.swift
+++ localNotif/ViewController.swift
@@ -7,19 +7,39 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var goBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: {didAlow, Error in
+            if didAlow {
+                print("Granted!")
+            } else {
+                print("No permission")
+            }
+            
+        })
+        goBtn.layer.cornerRadius = 45
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    @IBAction func goBtnTapped(_ sender: UIButton) {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Go"
+        content.subtitle = "Hello"
+        content.body = "Some random text"
+        content.badge = 1
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: "Timer done", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
 
 }
 
